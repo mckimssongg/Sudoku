328462086d16e130e9b46dba4d41c5c18bd08ea6
mckimssongg <86619053+mckimssongg@users.noreply.github.com>
2022-05-07 09:33:02 -0600

    add

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from logic.generatorBoard import matriz, tests
 
 
 # crear tabla de sudoku y dibujar con tkinter
-def create_table(Matriz):
+def main(Matriz):
     root = Tk()
     root.geometry("500x500")
+    root.maxsize(500, 500)
+    root.minsize(500, 500)
     root.title("Sudoku")
-    root.resizable(0, 0)
     for i in range(9):
         for j in range(9):
             # crear una caja para cada numero
@@ -16,14 +17,23 @@ def create_table(Matriz):
             entry.grid(row=i, column=j)
             entry.config(font=("Arial", 20))
             entry.insert(0, Matriz[i][j])
-
             # Editar solo los numeros que no esten vacios
             if Matriz[i][j] != "":
                 entry.config(state="disabled")
             else:
                 entry.config(state="normal")
 
+    # crear una un boton para verificar la solucion con la funcion tests
+    button = ttk.Button(root, text="Verificar",
+                        command=lambda: print(tests(Matriz)))
+    button.grid(row=9, column=0, columnspan=9)
+    button.config(width=20)
+
+    message = ttk.Label(
+        root, text="Presione el boton para verificar la solucion")
+    message.grid(row=10, column=0, columnspan=9)
+
     root.mainloop()
 
 
-create_table(matriz())
+main(matriz())
